dc8a5e0f2be881c3e4f4d1961dfef7313ff75460
likevanilla <wjdgur27507@gmail.com>
2025-11-02 16:46:26 +0900

    플리곤 클릭 이벤트 구현(마커와 라벨 생성코드 제거 버전)

--- kakaomap/src/App.jsx
+++ kakaomap/src/App.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
-import { Map, Polygon, MapMarker } from "react-kakao-maps-sdk";
+import React, { useState, useEffect } from "react";
+import { Map, Polygon } from "react-kakao-maps-sdk";
 import "./App.css";
 
 export default function App() {
   const [mapCenter] = useState({ lat: 37.566826, lng: 126.9786567 });
-  const [isOpen, setIsOpen] = useState(false);
+  const [isMouseOver, setIsMouseOver] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const handlePolygonClick = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    console.log(`다각형이 ${count}번 클릭되었습니다.`);
+  }, [count]);
 
   return (
     <div className="mapWrapper">
@@ -27,8 +36,13 @@ export default function App() {
           strokeWeight={2} // 선의 두께입니다
           strokeColor={"#000000"} // 선의 색깔입니다
           strokeOpacity={0.8} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-          fillColor={"#ffac13"} // 채우기 색깔입니다
-          fillOpacity={0.5} // 채우기 불투명도 입니다
+          fillColor={isMouseOver ? "#ff4000" : "#ffac13"} // 채우기 색깔입니다
+          fillOpacity={isMouseOver ? 0.8 : 0.5} // 채우기 불투명도 입니다
+          onMouseover={() => setIsMouseOver(true)}
+          onMouseout={() => setIsMouseOver(false)}
+          onMousedown={() => {
+            handlePolygonClick();
+          }}
         />
         {/* 오류 2동 다각형(폴리곤) */}
         {/* <Polygon
@@ -146,37 +160,6 @@ export default function App() {
           fillColor={"#775cf1"} // 채우기 색깔입니다
           fillOpacity={0.5} // 채우기 불투명도 입니다
         /> */}
-
-        <MapMarker
-          position={{
-            // 인포윈도우가 표시될 위치
-            lat: 37.48750415432594,
-            lng: 126.82580887749724,
-          }}
-          clickable={true} // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
-          onClick={() => setIsOpen(true)}
-        >
-          {/* MapMarker의 자식을 넣어줌으로 해당 자식이 InfoWindow로 만들어지게 합니다 */}
-          {/* 인포윈도우에 표출될 내용으로 HTML 문자열이나 React Component가 가능합니다 */}
-          {isOpen && (
-            <div style={{ minWidth: "150px" }}>
-              <img
-                alt="close"
-                width="14"
-                height="13"
-                src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
-                style={{
-                  position: "absolute",
-                  right: "5px",
-                  top: "5px",
-                  cursor: "pointer",
-                }}
-                onClick={() => setIsOpen(false)}
-              />
-              <div style={{ padding: "5px", color: "#000" }}>성공회대학교</div>
-            </div>
-          )}
-        </MapMarker>
       </Map>
     </div>
   );
